a136d401272d2b967b64c7918d035a9013e2f6bc
akashshelkande <akashshelkande@gmail.com>
2025-08-29 14:22:18 +0530

    Removed redundent code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,6 @@
-/*
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
-*/
-
 import React, { useEffect, useMemo, useState } from "react";
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
+
 // --- Configure your backend base URL here ---
 // If your Go server runs on http://localhost:8080 keep as-is.
 //const BASE_URL = "http://localhost:8080";
